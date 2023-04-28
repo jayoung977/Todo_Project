0be38ebe6a6c5790ae64a5458c22ab3cd66fa672
jayoung977 <wjsrnr20@gmail.com>
2023-04-28 16:41:01 +0900

    change var name & download commentary file

--- server/controller/Ctodo.js
+++ server/controller/Ctodo.js
@@ -3,28 +3,28 @@ exports.main = (req, res) => {
   res.send("hi");
 };
 // (1)  GET /todos - show all todo
-exports.getTodo = async (req, res) => {
+exports.readTodos = async (req, res) => {
   const result = await models.Todo.findAll();
-  console.log("(1) Ctodo.js getTodo findAll >> ", result);
+  console.log("(1) Ctodo.js readTodos findAll >> ", result);
   res.send(result);
   // res.render("todos", { data: result });
 };
 // (2) POST /todo - create a new todo
-exports.postTodo = async (req, res) => {
+exports.createTodo = async (req, res) => {
   const result = await models.Todo.create({
     id: req.body.id,
     title: req.body.title,
     done: req.body.done,
   });
-  console.log("(2)  Ctodo.js postTodo create >> ", result);
+  console.log("(2)  Ctodo.js createTodo create >> ", result);
   res.send(result);
 };
 
 // (3) PATCH /todo/:todoId - edit a specific todo
 
-exports.patchTodo = async (req, res) => {
+exports.updateTodo = async (req, res) => {
   // req.params.id
-  console.log("(3) Ctodo.js patchTodo req.params.id", req.params.id);
+  console.log("(3) Ctodo.js updateTodo req.params.id", req.params.id);
   const result = await models.Todo.update(
     {
       id: req.params.todoId,
@@ -35,7 +35,7 @@ exports.patchTodo = async (req, res) => {
       where: { id: req.params.todoId },
     }
   );
-  console.log(" (3) Ctodo.js patchTodo update >>", result);
+  console.log(" (3) Ctodo.js updateTodo update >>", result);
   res.end();
 };
 
